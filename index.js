4f78dcdf2f12f03b23016f37472c4fae8c9ae012
Max <kisjlotaytt@gmail.com>
2026-09-18 19:43:34 +0200

    Demo interaktiv

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const pole = [4, 19, 2, 200, 10, 13];
-let ncislo = pole[0];
-for(const nclo of pole){
-    if(nclo < ncislo){
-        ncislo = nclo;
-    }
-}
-console.log("Nejm. cislo pole: " + ncislo)
+const container = document.getElementById('inter');
+const contents = document.querySelectorAll('.');
